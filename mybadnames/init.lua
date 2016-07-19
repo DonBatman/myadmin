cfea34f9b374e901cc0e0e23cc13c3e69af47f03
DonBatman <serfdon@gmail.com>
2016-07-18 18:19:19 -0700

    fixed error

--- mybadnames/init.lua
+++ mybadnames/init.lua
@@ -18,8 +18,10 @@ for _, nm in pairs(bad_names) do
 minetest.register_on_prejoinplayer(function(name, ip)
 	local n = string.find(string.lower(name),nm)
 	local wp, err = io.open(minetest.get_worldpath().."/players/"..name, "r")
-	if wp then return end
-	wp:close()
+	if wp then 
+		wp:close()
+		return
+	end
 	if n ~= nil then
 		return "\nThe name .."..name.." is not allowed\n\nPlease pick a different Name."
 	end
