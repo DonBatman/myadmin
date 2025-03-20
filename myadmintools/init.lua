7042a45177bc4009bc5aa3e242b3ed0688c82805
Don <serfdon@gmail.com>
2025-03-19 21:57:52 -0700

    Updated Files

--- myadmintools/init.lua
+++ myadmintools/init.lua
@@ -1,66 +1,51 @@
+local admin_privilege = minetest.settings:get("myadmintools.admin_privilege") or "server"
 
-minetest.register_alias("ut", "myadmintools:ultimate_tool")
-minetest.register_alias("utd", "myadmintools:ultimate_tool_drop")
+local function register_ultimate_tool(name, description, image, drops)
+    minetest.register_tool(name, {
+        description = description,
+        inventory_image = image,
+        groups = {not_in_creative_inventory = 1},
+        tool_capabilities = {
+            full_punch_interval = 0.2,
+            max_drop_level = 3,
+            groupcaps = {
+                unbreakable = {times = {[1] = 0, [2] = 0, [3] = 0}, uses = 0, maxlevel = 3},
+                fleshy = {times = {[1] = 0, [2] = 0, [3] = 0}, uses = 0, maxlevel = 3},
+                choppy = {times = {[1] = 0, [2] = 0, [3] = 0}, uses = 0, maxlevel = 3},
+                bendy = {times = {[1] = 0, [2] = 0, [3] = 0}, uses = 0, maxlevel = 3},
+                cracky = {times = {[1] = 0, [2] = 0, [3] = 0}, uses = 0, maxlevel = 3},
+                crumbly = {times = {[1] = 0, [2] = 0, [3] = 0}, uses = 0, maxlevel = 3},
+                snappy = {times = {[1] = 0, [2] = 0, [3] = 0}, uses = 0, maxlevel = 3},
+            }
+        },
+        on_drop = function(itemstack, dropper, pos)
+            if not drops then
+              dropper:remove_item(itemstack);
+              minetest.chat_send_player(dropper:get_player_name(), "This tool cannot be dropped.")
+            end
+        end,
+    })
+end
 
-minetest.register_tool("myadmintools:ultimate_tool", {
-	description = "Ultimate Tool",
-	inventory_image = "ultimate_tool.png",
-	groups = {not_in_creative_inventory=1},
-	tool_capabilities = {
-		full_punch_interval = 0.2,
-		max_drop_level=3,
-		groupcaps= {
-			unbreakable={times={[1]=0, [2]=0, [3]=0}, uses=0, maxlevel=3},
-			fleshy = {times={[1]=0, [2]=0, [3]=0}, uses=0, maxlevel=3},
-			choppy={times={[1]=0, [2]=0, [3]=0}, uses=0, maxlevel=3},
-			bendy={times={[1]=0, [2]=0, [3]=0}, uses=0, maxlevel=3},
-			cracky={times={[1]=0, [2]=0, [3]=0}, uses=0, maxlevel=3},
-			crumbly={times={[1]=0, [2]=0, [3]=0}, uses=0, maxlevel=3},
-			snappy={times={[1]=0, [2]=0, [3]=0}, uses=0, maxlevel=3},
-		}
-	},
-	on_drop = function(itemstack, dropper, pos)
-	end
-})
-minetest.register_tool("myadmintools:ultimate_tool_drop", {
-	description = "Ultimate Tool With Drops",
-	inventory_image = "ultimate_tool2.png",
-	groups = {not_in_creative_inventory=1},
-	tool_capabilities = {
-		full_punch_interval = 0.2,
-		max_drop_level=3,
-		groupcaps= {
-			unbreakable={times={[1]=0, [2]=0, [3]=0}, uses=0, maxlevel=3},
-			fleshy = {times={[1]=0, [2]=0, [3]=0}, uses=0, maxlevel=3},
-			choppy={times={[1]=0, [2]=0, [3]=0}, uses=0, maxlevel=3},
-			bendy={times={[1]=0, [2]=0, [3]=0}, uses=0, maxlevel=3},
-			cracky={times={[1]=0, [2]=0, [3]=0}, uses=0, maxlevel=3},
-			crumbly={times={[1]=0, [2]=0, [3]=0}, uses=0, maxlevel=3},
-			snappy={times={[1]=0, [2]=0, [3]=0}, uses=0, maxlevel=3},
-		}
-	},
-	on_drop = function(itemstack, dropper, pos)
-	end
-})
+register_ultimate_tool("myadmintools:ut", "Ultimate Tool", "ultimate_tool.png", false) -- No drops
+register_ultimate_tool("myadmintools:utd", "Ultimate Tool (Drops)", "ultimate_tool2.png", true) -- Drops allowed
+
+minetest.register_alias("ut", "myadmintools:ut")
+minetest.register_alias("utd", "myadmintools:utd")
 
 minetest.register_on_punchnode(function(pos, node, puncher)
-	local n = node
-	if puncher:get_wielded_item():get_name() == "myadmintools:ultimate_tool"
-	and minetest.get_node(pos).name ~= "air" 
-	and minetest.get_player_privs(puncher:get_player_name()).myprivs_levels_super == true then
-		minetest.remove_node(pos)
-	end
+    local player_name = puncher:get_player_name()
 
-	if puncher:get_wielded_item():get_name() == "myadmintools:ultimate_tool"
-	or puncher:get_wielded_item():get_name() == "myadmintools:ultimate_tool_drop"
-	and minetest.get_node(pos).name ~= "air" then
-		if minetest.get_player_privs(puncher:get_player_name()).myprivs_levels_super ~= true then
-			minetest.chat_send_player(puncher:get_player_name(), "You don't have the priv for this tool")
-			puncher:set_wielded_item("default:stick")
-			minetest.set_node(pos,{name = node.name})
-			return
-		end
-	end
+    if not minetest.get_player_privs(player_name)[admin_privilege] then
+        minetest.chat_send_player(player_name, "You don't have the required privilege for this tool.")
+        puncher:set_wielded_item("default:stick")
+        return
+    end
+
+    local wielded_item = puncher:get_wielded_item()
+    if wielded_item:get_name() == "myadmintools:ut" or wielded_item:get_name() == "myadmintools:utd" then
+        if node.name ~= "air" then
+            minetest.remove_node(pos)
+        end
+    end
 end)
-
-
