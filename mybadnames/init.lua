9ca6c272a26857cee8c6b662a0a2acc088bd87a0
Don <serfdon@gmail.com>
2025-03-19 21:58:32 -0700

    updated files

--- mybadnames/init.lua
+++ mybadnames/init.lua
@@ -1,31 +1,37 @@
 local bad_names = {
-		"fuck",
-		"faggot",
-		"asshole",
-		"arsehole",
-		"cunt",
-		"retard",
-		"bitch",
-		"shit",
-		"bastard",
-		"nigger",
-		"nigga",
-		"guest",
-		"sadie"
-		}
+    "fuck",
+    "faggot",
+    "asshole",
+    "arsehole",
+    "cunt",
+    "retard",
+    "bitch",
+    "shit",
+    "bastard",
+    "nigger",
+    "nigga",
+    "guest"
+}
 
-for _, nm in pairs(bad_names) do
+local function normalize_name(name)
+    return string.lower(name:gsub("[^%w]", ""))
+end
 
 minetest.register_on_prejoinplayer(function(name, ip)
-	local n = string.find(string.lower(name),nm)
-	local wp, err = io.open(minetest.get_worldpath().."/players/"..name, "r")
-	if wp then 
-		wp:close()
-		return
-	end
-	if n ~= nil then
-		return "\nThe name .."..name.." is not allowed\n\nPlease pick a different Name."
-	end
-end)
+    local normalized_name = normalize_name(name)
 
-end
+    local player_file = minetest.get_worldpath() .. "/players/" .. name
+    local file, err = io.open(player_file, "r")
+    if file then
+        file:close()
+        return
+    end
+
+    for _, bad_name in ipairs(bad_names) do
+        if string.find(normalized_name, bad_name) then
+            return "\nYour chosen name is not allowed. Please choose a different name."
+        end
+    end
+
+    return
+end)
